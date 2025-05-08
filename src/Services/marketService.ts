762ba288aefb19dbc37ef951d4a1b18a30a4089a
Luca Nora <luca.nora.ext@jakala.com>
2025-05-08 15:12:42 +0200

    feat: add product management functionality to MarketService
    - Added `addProduct` method to MarketService for adding new products.
    - Implemented `saveProducts` method to persist product data.
    - Updated tests to cover the new product addition functionality.
    - Included TypeScript as a dev dependency in package.json.

--- src/Services/marketService.ts
+++ src/Services/marketService.ts
@@ -1,7 +1,8 @@
 import { Product } from "../Models/product";
-import { readJson } from "../Utils/utils";
+import { readJson, writeJson } from "../Utils/utils";
 
 export default class MarketService {
+
   public getAllProducts(): Product[] {
     const rooms: Product[] = readJson("products.json");
     return rooms;
@@ -15,5 +16,21 @@ export default class MarketService {
     } else {
       return null;
     }
+  }  
+  
+  public addProduct(name: string, price: number) : Product {
+    const products = this.getAllProducts();
+    const newId = products.length > 0 ? products[products.length - 1].id + 1 : 1;
+    const newProduct: Product = {
+      id: newId,
+      name: name,
+      price: price,
+    };
+    products.push(newProduct);
+    return newProduct
+  }
+
+  private saveProducts(products: Product[]) {
+    writeJson(products, "products.json");
   }
 }
--- src/Tests/marketService.test.ts
+++ src/Tests/marketService.test.ts
@@ -9,3 +9,18 @@ it('Test Caricamento Banana', () => {
     expect(product?.name).toBe("Banana");
 	expect(product?.price).toBe(2);
   });
+
+// Aggiunta singolo prodotto con prezzo fisso
+it('Test Aggiunta Prodotto Singolo', () => {
+    var service = new marketService();
+    var product = service.addProduct("Banana", 2);
+    expect(product).not.toBeNull();
+    expect(product?.name).toBe("Banana");
+    expect(product?.price).toBe(2);
+    // id non deve essere null
+    expect(product?.id).not.toBeNull();
+    // id deve essere un numero
+    expect(typeof product?.id).toBe("number");
+    // id deve essere maggiore di 0
+    expect(product?.id).toBeGreaterThan(0);
+  });
